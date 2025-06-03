d8f6fb06c3deb59a42a40b4d252103218e62bf67
Mineeva Daria <daramineeva345@gmail.com>
2025-06-03 22:26:09 +0300

    build(app_details):

--- blog/urls.py
+++ blog/urls.py
@@ -1,18 +1,21 @@
 from django.urls import path
 from . import views
+from .views import ApplicationDeleteView, ApplicationListView, ApplicationDetailView, ApplicationCreateView, ApplicationUpdateView, ApplicationDeleteView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('applications/', views.ApplicationListView.as_view(), name='application-list'),
-    path('applications/<int:pk>/', views.ApplicationDetailView.as_view(), name='application-detail'),
-    path('applications/new/', views.ApplicationCreateView.as_view(), name='application-create'),
-    path('applications/<int:pk>/update/', views.ApplicationUpdateView.as_view(), name='application-update'),
+    path('applications/', ApplicationListView.as_view(), name='application-list'),
+    path('application/<int:pk>/', ApplicationDetailView.as_view(), name='application-detail'),
+    path('application/create/', ApplicationCreateView.as_view(), name='application-create'),
+    path('applications/<int:pk>/update/', ApplicationUpdateView.as_view(), name='application-update'),
     path('applications/<int:app_id>/upload/', views.upload_document, name='document-upload'),
     path('notifications/', views.NotificationListView.as_view(), name='notification-list'),
     path('notifications/<int:pk>/read/', views.mark_notification_read, name='mark-notification-read'),
     path('profile/', views.profile, name='profile'),
     path('auditors/', views.AuditorListView.as_view(), name='auditor-list'),
     path('applications/search/', views.ApplicationSearchView.as_view(), name='application-search'),
+    path('application/<int:pk>/delete/', ApplicationDeleteView.as_view(), name='application-delete'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,15 +4,28 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import User, Organization, AuditorCompany, Application, Document, Notification
 from .forms import ApplicationForm, DocumentForm 
-from django.db.models import Count, Q
+from django.db.models import Count, Q, Avg, Case, When, FloatField
 from django.contrib import messages
 from django.http import JsonResponse
+from django.views.generic import DeleteView
+from django.urls import reverse_lazy
 
 def home(request):
+    completed_percentage = Application.objects.aggregate(
+        avg_completed=Avg(
+            Case(
+                When(status='Завершено', then=100),
+                default=0,
+                output_field=FloatField()
+            )
+        )
+    )['avg_completed'] or 0
+
     context = {
         'organizations_count': Organization.objects.count(),
         'auditors_count': AuditorCompany.objects.count(),
         'applications_count': Application.objects.count(),
+        'completed_percentage': round(completed_percentage, 1),
         'latest_applications': Application.objects.select_related(
             'organization', 'auditor_company'
             ).order_by('-date')[:10],
@@ -31,7 +44,6 @@ class ApplicationListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         queryset = Application.objects.select_related('organization', 'auditor_company').order_by('-date')
         
-        # Фильтрация по роли пользователя
         if self.request.user.role == 'Организация':
             queryset = queryset.filter(organization__user=self.request.user)
         elif self.request.user.role == 'Аудитор':
@@ -39,7 +51,6 @@ class ApplicationListView(LoginRequiredMixin, ListView):
         elif self.request.user.role != 'Администратор':
             queryset = Application.objects.none()
         
-        # Добавляем поддержку поиска
         query = self.request.GET.get('q', '').strip()
         if query:
             queryset = queryset.filter(
@@ -58,7 +69,7 @@ class ApplicationListView(LoginRequiredMixin, ListView):
 class ApplicationDetailView(LoginRequiredMixin, DetailView):
     model = Application
     template_name = 'blog/application_detail.html'
-
+    success_url = reverse_lazy('blog:application-list')
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['documents'] = Document.objects.filter(application=self.object)
@@ -69,6 +80,7 @@ class ApplicationCreateView(LoginRequiredMixin, CreateView):
     model = Application
     form_class = ApplicationForm
     template_name = 'blog/application_form.html'
+    success_url = reverse_lazy('blog:application-list')
 
     def form_valid(self, form):
         form.instance.organization = get_object_or_404(Organization, user=self.request.user)
@@ -78,7 +90,9 @@ class ApplicationUpdateView(LoginRequiredMixin, UpdateView):
     model = Application
     form_class = ApplicationForm
     template_name = 'blog/application_form.html'
+    success_url = reverse_lazy('blog:application-list')
 
+    
 @login_required
 def upload_document(request, app_id):
     application = get_object_or_404(Application, pk=app_id)
@@ -162,3 +176,15 @@ class ApplicationSearchView(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['search_query'] = self.request.GET.get('q', '')
         return context
+
+class ApplicationDeleteView(LoginRequiredMixin, DeleteView):
+    model = Application
+    template_name = 'blog/application_confirm_delete.html'
+    success_url = reverse_lazy('blog:application-list')
+
+    def dispatch(self, request, *args, **kwargs):
+            app = self.get_object()
+            # Только владелец может удалить
+            if request.user != app.organization.user and request.user.role != 'Администратор':
+                return redirect('blog:application-list')
+            return super().dispatch(request, *args, **kwargs)
